3a6fb9d3f23278e44c9df4968a162107d6c59d0d
Sara Daccache <saracampg@hotmail.com>
2022-04-22 19:57:00 +0100

    Creating a container.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
 
-$app = [];
-$app['config'] = require 'config.php';
-
 
 require 'core\database\Connection.php';
 require 'core\database\QueryBuilder.php';
 require 'core\Router.php';
 require 'core\Request.php';
+require 'core\App.php';
 
+App::bind('config', require 'config.php');
 
-$app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
+App::bind('database', new QueryBuilder(Connection::make(App::get('config')['database'])));
